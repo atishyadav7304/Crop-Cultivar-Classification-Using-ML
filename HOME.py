e798a7b59a06421ea933b88a19807a5ef480f5ca
atishyadav7304 <atishyadav7304@gmail.com>
2026-08-19 15:02:06 +0530

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
-
+import streamlit.components.v1 as components
 # --- Page Configuration ---
 st.set_page_config(
     page_title="Crop Cultivar Identification",
